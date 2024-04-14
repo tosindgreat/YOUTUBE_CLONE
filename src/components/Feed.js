1bc5eaba7c29dd261a54ffbb568f43a3c38a1abc
oluwatosin amodu <tobunmi2002@yahoo.com>
2024-04-14 09:23:26 -0500

    Commit message

---
+++ src/components/Feed.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import fetchFromApi from "../utils/fetch-from-api";
+
+const Feed = () => {
+  const [category, setCategory] = useState("new");
+  const [data, setData] = useState({});
+
+  const url = `search?part=snippet&q=${category}`;
+
+  useEffect(() => {
+    const fetchFeedData = async () => {
+      try {
+        const response = await fetchFromApi(url);
+        setData(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchFeedData();
+  }, [url]);
+
+  return (
+    <>
+      {data.items.map((item) => (
+        <h1>{item.id.videoId}</h1>
+      ))}
+    </>
+  );
+};
+
+export default Feed;
